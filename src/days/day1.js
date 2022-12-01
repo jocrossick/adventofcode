615216a6e006bd964ef57faf616de558505d767d
Josephine Crossick <jocrossick@gmail.com>
2022-12-01 09:52:32 +0100

    Moar refactorz less linesz

--- src/days/day1.js
+++ src/days/day1.js
@@ -1,27 +1,20 @@
 const day1 = (input, part) => {
-  const addedCalories = [];
+  const topIndex = part == 1 ? 0 : 2;
+  let addedCalories = new Array(topIndex + 1);
+  addedCalories.fill(0);
   input
     .map((a) => parseInt(a))
     .reduce(
       (acc, curr) =>
-        isNaN(curr) ? Math.min(addedCalories.push(acc), 0) : acc + curr,
+        isNaN(curr)
+          ? addedCalories.sort((a, b) => a - b)[0] >= acc
+            ? 0
+            : Math.min((addedCalories[0] = acc), 0)
+          : acc + curr,
       0
     );
 
-  const sortedAddedCalories = addedCalories.sort((a, b) => a - b);
-  const part1Solution = sortedAddedCalories[addedCalories.length - 1];
-  const part2Solution =
-    sortedAddedCalories[addedCalories.length - 1] +
-    sortedAddedCalories[addedCalories.length - 2] +
-    sortedAddedCalories[addedCalories.length - 3];
-
-  console.log("Part 1 is", part1Solution);
-  console.log("Part 2 is", part2Solution);
-
-  if (part == 2) {
-    return part2Solution;
-  }
-  return part1Solution;
+  return addedCalories.reduce((acc, curr) => acc + curr);
 };
 
 module.exports = { day1 };
