f96b47c2f0f42015699d57bf334ff333d63b77d6
Josephine Crossick <jocrossick@gmail.com>
2022-12-01 08:29:30 +0100

    First pass at day 1

--- src/days/day1.js
+++ src/days/day1.js
@@ -1,8 +1,22 @@
 const { readFile } = require("../files/read");
 
 const day1 = () => {
+
+    const addedArray = [];
+    readFile("day1").reduce((acc, curr) => {
+        const thisInt = parseInt(curr);
+        if (isNaN(thisInt)) {
+            addedArray.push(acc);
+            return 0;
+        }
+        return acc + thisInt;
+    }, 0);
+
+    console.log(addedArray);
+
+    const sortedArray = (addedArray.sort((a, b) => a - b));
     
-    console.log(readFile("day1"));
+    return sortedArray[addedArray.length-1] + sortedArray[addedArray.length-2]+ sortedArray[addedArray.length-3];
 }
 
 module.exports = { day1 }
--- src/days/day1.test.js
+++ src/days/day1.test.js
@@ -3,7 +3,7 @@ const { day1 } = require("./day1");
 describe("day1", () => {
 
   test("example 1", () => {
-    day1();
+    console.log(day1());
   });
-  
+
 });
