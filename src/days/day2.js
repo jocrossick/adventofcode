a6b787c6d590dab692299111664f31e7b9c7e90f
Josephine Crossick <jocrossick@gmail.com>
2022-12-02 11:21:28 +0100

    Some tidying up

--- src/days/day2.js
+++ src/days/day2.js
@@ -3,49 +3,32 @@
 const lose = 0;
 const draw = 3;
 const win = 6;
-const lookups1 = {
-  A: 1, //"rock",
-  B: 2, //"paper",
-  C: 3, //"scissors",
-  X: 1, //"rock",
-  Y: 2, //"paper",
-  Z: 3, //"scissors",
-  //   rock: 1,
-  //   paper: 2,
-  //   scissors: 3,
-};
-const lookups2 = {
-  A: 1, //"rock",
-  B: 2, //"paper",
-  C: 3, //"scissors",
-  X: 0, //"lose",
-  Y: 3, //"draw",
-  Z: 6, //"win",
-  //   rock: 1,
-  //   paper: 2,
-  //   scissors: 3,
-};
-//win
-//1 -> 2
-//2 -> 3
+const lookups = [
+  {
+    A: 1, //"rock",
+    B: 2, //"paper",
+    C: 3, //"scissors",
+    X: 1, //"rock",
+    Y: 2, //"paper",
+    Z: 3, //"scissors",
+  },
+  {
+    A: 1, //"rock",
+    B: 2, //"paper",
+    C: 3, //"scissors",
+    X: 0, //"lose",
+    Y: 3, //"draw",
+    Z: 6, //"win",
+  },
+];
 
-// 1, 2 - win(1); //3
-// 2, 3 - win(1); //5
-// 3, 1 - win(-2); //4
-// 1, 3 - lose(2); //4
-// 2, 1 - lose(-1);
-// 3, 2 - lose(-1);
-
-const day2pt1 = (input) =>
+const day2 = (input, part) =>
   input
-    .map((entry) => [lookups1[entry.charAt(0)], lookups1[entry.charAt(2)]])
-    .map((entry) => winner(entry[0], entry[1]) + entry[1])
-    .reduce((acc, curr) => acc + curr);
-
-const day2pt2 = (input) =>
-  input
-    .map((entry) => [lookups2[entry.charAt(0)], lookups2[entry.charAt(2)]])
-    .map((entry) => play(entry[0], entry[1]) + entry[1])
+    .map((entry) => [
+      lookups[part - 1][entry.charAt(0)],
+      lookups[part - 1][entry.charAt(2)],
+    ])
+    .map((entry) => score(part)(entry[0], entry[1]) + entry[1])
     .reduce((acc, curr) => acc + curr);
 
 const winner = (them, you) => {
@@ -63,9 +46,11 @@ const play = (them, strategy) => {
     return them;
   }
   if (strategy == win) {
-    return (them + 1) < 4 ? (them + 1) : (them + 1) % 3;
+    return them + 1 < 4 ? them + 1 : (them + 1) % 3;
   }
-  return (them + 2) < 4 ? (them + 2) : (them + 2) % 3;
+  return them + 2 < 4 ? them + 2 : (them + 2) % 3;
 };
 
-module.exports = { day2pt1, day2pt2 };
+const score = (part) => (part == 1 ? winner : play);
+
+module.exports = { day2 };
