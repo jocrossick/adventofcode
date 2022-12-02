e13788d252beba17aaf6728f4fdf10c5c7ac9d6d
Josephine Crossick <jocrossick@gmail.com>
2022-12-02 10:43:05 +0100

    part 2 fixed

--- src/days/day2.js
+++ src/days/day2.js
@@ -3,38 +3,69 @@
 const lose = 0;
 const draw = 3;
 const win = 6;
-const lookups = {
+const lookups1 = {
   A: 1, //"rock",
   B: 2, //"paper",
   C: 3, //"scissors",
   X: 1, //"rock",
   Y: 2, //"paper",
   Z: 3, //"scissors",
-//   rock: 1,
-//   paper: 2,
-//   scissors: 3,
+  //   rock: 1,
+  //   paper: 2,
+  //   scissors: 3,
 };
-// 1, 2 - win(1);
-// 2, 3 - win(1);
-// 3, 1 - win(-2);
-// 1, 3 - lose(2);
+const lookups2 = {
+  A: 1, //"rock",
+  B: 2, //"paper",
+  C: 3, //"scissors",
+  X: 0, //"lose",
+  Y: 3, //"draw",
+  Z: 6, //"win",
+  //   rock: 1,
+  //   paper: 2,
+  //   scissors: 3,
+};
+//win
+//1 -> 2
+//2 -> 3
+
+// 1, 2 - win(1); //3
+// 2, 3 - win(1); //5
+// 3, 1 - win(-2); //4
+// 1, 3 - lose(2); //4
 // 2, 1 - lose(-1);
 // 3, 2 - lose(-1);
 
-const day2 = (input) =>
+const day2pt1 = (input) =>
   input
-    .map((entry) => [lookups[entry.charAt(0)], lookups[entry.charAt(2)]])
+    .map((entry) => [lookups1[entry.charAt(0)], lookups1[entry.charAt(2)]])
     .map((entry) => winner(entry[0], entry[1]) + entry[1])
     .reduce((acc, curr) => acc + curr);
 
+const day2pt2 = (input) =>
+  input
+    .map((entry) => [lookups2[entry.charAt(0)], lookups2[entry.charAt(2)]])
+    .map((entry) => play(entry[0], entry[1]) + entry[1])
+    .reduce((acc, curr) => acc + curr);
+
 const winner = (them, you) => {
-  if (them == you) {
+  if (them - you == 0) {
     return draw;
   }
-  if (you - them == 1 || you - them == -2) {
+  if ((you - them + 3) % 3 == 1) {
     return win;
   }
   return lose;
 };
 
-module.exports = { day2 };
+const play = (them, strategy) => {
+  if (strategy == draw) {
+    return them;
+  }
+  if (strategy == win) {
+    return (them + 1) < 4 ? (them + 1) : (them + 1) % 3;
+  }
+  return (them + 2) < 4 ? (them + 2) : (them + 2) % 3;
+};
+
+module.exports = { day2pt1, day2pt2 };
--- src/days/day2.test.js
+++ src/days/day2.test.js
@@ -1,26 +1,26 @@
 const { readFile } = require("../files/read");
-const { day2 } = require("./day2");
+const { day2pt1, day2pt2 } = require("./day2");
 
 describe("day2 test runs", () => {
     test("day 2 part 1", () => {
-      const day2Result = day2(readFile("day2-test1"));
+      const day2Result = day2pt1(readFile("day2-test1"));
       expect(day2Result).toStrictEqual(15);
     });
   
-    xtest("day 2 part 2", () => {
-      const day2Result = day2(readFile("day2-test1"), 2);
-      expect(day2Result).toStrictEqual(45000);
+    test("day 2 part 2", () => {
+      const day2Result = day2pt2(readFile("day2-test1"));
+      expect(day2Result).toStrictEqual(12);
     });
   });
 
 describe("day2 solution", () => {
   test("day 2 part 1", () => {
-    const day2Result = day2(readFile("day2"), 1);
+    const day2Result = day2pt1(readFile("day2"));
     expect(day2Result).toStrictEqual(12855);
   });
 
-  xtest("day 2 part 2", () => {
-    const day2Result = day2(readFile("day2"), 2);
-    expect(day2Result).toStrictEqual(208567);
+  test("day 2 part 2", () => {
+    const day2Result = day2pt2(readFile("day2"));
+    expect(day2Result).toStrictEqual(11797); //too low
   });
 });
