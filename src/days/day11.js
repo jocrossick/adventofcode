07087ada7f6ee83ab424c5b1f96a5a734379b4fa
Josephine Crossick <jocrossick@gmail.com>
2022-12-18 15:52:07 +0100

    Day 11 part 1

--- src/days/day11.js
+++ src/days/day11.js
@@ -1,4 +1,78 @@
-const day11 = (input, part) => {
-    console.log("Not implemented");
-}
-module.exports = { day11 }
+const monkeyNumRegex = /(?<startText>Monkey )(?<monkeyNum>[0-9]*)/;
+const startingItemsRegex =
+  /(?<startText>  Starting items: )(?<itemsList>[0-9\,\ ]*)/;
+const operationRegex =
+  /(?<startText>  Operation: new = )(?<operation>old [+-\/\*] [a-z,A-Z,0-9]*)/;
+const testRegex = /(?<startText>  Test: divisible by )(?<divisibleBy>[0-9]*)/;
+const ifTrueRegex =
+  /(?<startText>    If true: throw to monkey )(?<toMonkeyIfTrue>[0-9])*/;
+const ifFalseRegex =
+  /(?<startText>    If false: throw to monkey )(?<toMonkeyIfFalse>[0-9])*/;
+
+const day11 = (input, maxRounds, part) => {
+  const monkeys = input
+    .reduce(
+      (acc, curr) => {
+        if (curr == "") {
+          acc.push([]);
+          return acc;
+        }
+        acc[acc.length - 1].push(curr);
+        return acc;
+      },
+      [[]]
+    )
+    .reduce((acc, monkeyArr) => {
+      acc[monkeyArr[0].match(monkeyNumRegex).groups.monkeyNum] =
+        monkeyParser(monkeyArr);
+      return acc;
+    }, {});
+
+  for (let i = 0; i < maxRounds; i++) {
+    monkeyRound(monkeys);
+  }
+  const inspectionCounts = Object.keys(monkeys).map(monkey => monkeys[monkey].monkeyInspectionCount).sort((a, b) => b - a);
+  return inspectionCounts[0]*inspectionCounts[1];
+};
+
+const monkeyParser = (monkeyArr) => {
+  const ifTrue = monkeyArr[4].match(ifTrueRegex).groups.toMonkeyIfTrue;
+  const ifFalse = monkeyArr[5].match(ifFalseRegex).groups.toMonkeyIfFalse;
+  const operation = Function(
+    "old",
+    "return " + monkeyArr[2].match(operationRegex).groups.operation
+  );
+  const test = parseInt(monkeyArr[3].match(testRegex).groups.divisibleBy);
+
+  return {
+    monkeyNum: monkeyArr[0].match(monkeyNumRegex).groups.monkeyNum,
+    items: monkeyArr[1]
+      .match(startingItemsRegex)
+      .groups.itemsList.split(", ")
+      .map((item) => parseInt(item)),
+    monkeyDecider: monkeyDecider(operation, test, ifTrue, ifFalse),
+    monkeyInspectionCount: 0
+  };
+};
+
+const monkeyDecider = (operationFunction, testVal, ifTrue, ifFalse) => {
+  return (item) => {
+    let worryLevel = Math.trunc(operationFunction(item) / 3);
+    passesTest = Math.trunc(worryLevel / testVal) == worryLevel / testVal;
+    const newMonkey = passesTest ? ifTrue : ifFalse;
+    return [worryLevel, newMonkey];
+  };
+};
+
+const monkeyRound = (monkeys) => {
+  Object.keys(monkeys).forEach((key) => {
+    monkeys[key].items.forEach((item) => {
+      const [newItemWorryLevel, newMonkey] = monkeys[key].monkeyDecider(item);
+      monkeys[newMonkey].items.push(newItemWorryLevel);
+    });
+    monkeys[key].monkeyInspectionCount = monkeys[key].monkeyInspectionCount + monkeys[key].items.length;
+    monkeys[key].items = [];
+  });
+};
+
+module.exports = { day11, monkeyParser };
--- src/days/day11.test.js
+++ src/days/day11.test.js
@@ -1,25 +1,69 @@
 const { readFile } = require("../files/read");
-const { day11 } = require("./day11");
+const { day11, monkeyParser } = require("./day11");
 
 describe("day11 test runs part 1", () => {
-  test("day 11 part 1 test 1", () => {
-    const result = day11(readFile("day11-test"), 1);
-    expect(result).toStrictEqual(1);
+  xtest("regex parsing test", () => {
+    const monkeyArr = [
+      "Monkey 0:",
+      "  Starting items: 79, 98",
+      "  Operation: new = old * 19",
+      "  Test: divisible by 23",
+      "    If true: throw to monkey 2",
+      "    If false: throw to monkey 3"];
+    let result = monkeyParser(monkeyArr);
+    const anonFunc = Function("old", "return old * 19");
+    //expect(result).toMatchObject({"ifFalse": "3", "ifTrue": "2", "items": [79, 98], "monkeyNum": "0", "operation": anonFunc, "test": "23"})
+    const testAdds =  Function("old", "return old + 1");
+    expect(testAdds(1)).toEqual(2);
+    const testSubtract =  Function("old", "return old - 1");
+    expect(testSubtract(10)).toEqual(9);
+    const testMultiply =  Function("old", "return old * 2");
+    expect(testMultiply(3)).toEqual(6);
+    const testDivide =  Function("old", "return old / 10");
+    expect(testDivide(100)).toEqual(10);
+
   });
 
-  test("day 11 part 2", () => {
+  xtest("day 11 part 1 test moves", () => {
+    const monkeys1 = day11(readFile("day11-test"), 1, 1);
+    expect(monkeys1[0].items).toStrictEqual([20, 23, 27, 26]);
+    expect(monkeys1[1].items).toStrictEqual([2080, 25, 167, 207, 401, 1046]);
+    expect(monkeys1[2].items).toStrictEqual([]);
+    expect(monkeys1[3].items).toStrictEqual([]);
+
+    const monkeys2 = day11(readFile("day11-test"), 2, 1);
+    expect(monkeys2[0].items).toStrictEqual([695, 10, 71, 135, 350]);
+    expect(monkeys2[1].items).toStrictEqual([43, 49, 58, 55, 362]);
+    expect(monkeys2[2].items).toStrictEqual([]);
+    expect(monkeys2[3].items).toStrictEqual([]);
+
+    const monkeys20 = day11(readFile("day11-test"), 20, 1);
+    expect(monkeys20[0].items).toStrictEqual([10, 12, 14, 26, 34]);
+    expect(monkeys20[1].items).toStrictEqual([245, 93, 53, 199, 115]);
+    expect(monkeys20[2].items).toStrictEqual([]);
+    expect(monkeys20[3].items).toStrictEqual([]);
+
+  });
+
+  test("Day 22 part 1 test",() => {
+    const monkeys20 = day11(readFile("day11-test"), 20, 1);
+    expect(monkeys20).toStrictEqual(10605);
+
+  })
+
+  xtest("day 11 part 2", () => {
     const result = day11(readFile("day11-test"), 2);
     expect(result).toStrictEqual(1);
    });
 });
 
-xdescribe("day11 answer", () => {
+describe("day11 answer", () => {
   test("day 11 part 1 answer", () => {
-    const result = day11(readFile("day11"), 1);
-    expect(result).toStrictEqual(14360);
+    const result = day11(readFile("day11"), 20, 1);
+    expect(result).toStrictEqual(1);
   });
 
-  test("day 11 part 2", () => {
+  xtest("day 11 part 2", () => {
     const result = day11(readFile("day11"), 2);
     expect(result).toStrictEqual(1);
   });
