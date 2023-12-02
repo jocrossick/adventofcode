c6f2310e0c19beff85a59115ee1d2804dca0fe46
Jo Crossick <jcrossick@hubspot.com>
2023-12-02 14:55:25 +0100

    Day 2 part 1

--- 2023/index.js
+++ 2023/index.js
@@ -1,3 +1,4 @@
 const { day1 } = require("./src/previous/day1/day1");
+const { day2 } = require("./src/today/day2/day2");
 
-day1();
+day2();
---
+++ 2023/src/today/day2/day2.js
@@ -0,0 +1,56 @@
+const { readFile } = require("../../useful/read");
+
+//12 red cubes, 13 green cubes, and 14 blue cubes
+const elfConditions = { red: 12, green: 13, blue: 14 }
+
+const parseInput = (filename) => {
+
+  const inputLines = readFile('day2', filename);
+  //Remove the last empty line from the file
+  inputLines.pop();
+
+  return inputLines.map(line => {
+    const [game, description] = line.split(': ');
+    const setsStringArr = description.split('; ');
+    const sets = setsStringArr.map(setString => {
+      const pickStringArr = setString.split(', ');
+      const picks = pickStringArr.reduce((pickJson, pickString) => {
+        const [qty, colour] = pickString.split(' ');
+        pickJson[colour] = qty;
+        return pickJson;
+      }, {});
+      return picks;
+    })
+    return { game: game.split(' ')[1], sets };
+  });
+}
+
+const testParseInput = () => {
+  const testResult = parseInput('test1');
+  console.assert(testResult[2].game == '3', "Did not have the right value in game" );
+  console.assert(testResult[2].sets[1].red == '4', "Did not have the right value in set 1 red" );
+}
+testParseInput();
+
+//Part 1: 2283
+const day2 = () => {
+  const allGames = parseInput('part1');
+  const elfConditionsKeys = Object.keys(elfConditions);
+  const possibleGames = allGames.filter(game => {
+    const possibleSets = game.sets.filter(set => {
+      let isPossible = true;
+      elfConditionsKeys.forEach(key => {
+        if (set[key] > elfConditions[key]) {
+          isPossible = false;
+        }
+      });
+      return isPossible;
+    })
+    return possibleSets.length == game.sets.length;
+  })
+
+  console.log("Solution is ", possibleGames.reduce((acc, game) => acc + parseInt(game.game), 0 ));
+
+}
+
+module.exports = { day2 }
