d91e4d63cbe49ce7d79604b3d953548010889df9
Jo Crossick <jcrossick@hubspot.com>
2024-12-02 08:18:53 +0100

    Day 2 part 1

--- 2024/read.js
+++ 2024/read.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const readFile = (isTest) => {
 
     if (isTest) {
-        return fs.readFileSync("today/input/test.txt", "utf-8").split("\n");
+        return fs.readFileSync("today/input/test.txt", "utf-8").split("\n").filter(line => line);
     }
 
-    return fs.readFileSync("today/input/input.txt", "utf-8").split("\n");
+    return fs.readFileSync("today/input/input.txt", "utf-8").split("\n").filter(line => line);
 
 }
 
--- 2024/today/today.js
+++ 2024/today/today.js
@@ -1,10 +1,44 @@
 const { readFile } = require("../read.js");
 
-const day1 = () => {
+const day2 = () => {
 
   const rawInput = readFile(false);
 
+  const data = rawInput.map((line, index) => {
+    if (line.length < 1) {
+      return;
+    }
+    const arr = line.split(' ');
+    let isDescending = true;
+    let isAscending = true;
+    let gapLessThan4 = true;
+
+    for (let i = 1; i< arr.length; i++) {
+      if (arr[i-1]-arr[i] >= 0) {
+        isAscending = false;
+      }
+      if (arr[i-1]-arr[i] <= 0) {
+        isDescending = false;
+      }
+      if (Math.abs(arr[i-1]-arr[i]) > 3) {
+        gapLessThan4 = false;
+      }
+    }
+    return {
+      lineNo: index,
+      arr,
+      isDescending,
+      isAscending,
+      gapLessThan4,
+      isSafe: (isAscending || isDescending) && gapLessThan4
+    }
+  });
+  console.log(data)
+
+  const safeLines = data.filter(entry => entry.isSafe);
+
+  console.log("Part 1: Number of safe lines: ", safeLines.length);
 }
 
-const today = () => day1();
+const today = () => day2();
 module.exports = { today };
